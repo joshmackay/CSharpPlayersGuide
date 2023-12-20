fc2096a7ee96366299b5fb789c3a157813e72f8d
Josh MacKay <josh.mackay@tgc.vic.edu.au>
2023-12-20 14:31:48 +1100

    updates

--- ThePasswordValidator/Program.cs
+++ ThePasswordValidator/Program.cs
@@ -1,7 +1,7 @@
 ﻿using System.Net.Http.Headers;
 using ThePasswordValidator;
 
-while (true)
+/*while (true)
 {
     Console.WriteLine("Please enter a password: ");
     string password = Console.ReadLine();
@@ -12,4 +12,22 @@ while (true)
         Console.WriteLine("Password is valid");
     }
     else Console.WriteLine("Password is invalid");
+}*/
+
+object a = new object();
+object b = new Point(2, 4);
+
+Console.WriteLine(a.GetHashCode());
+Console.WriteLine(b.GetHashCode());
+
+Console.WriteLine(typeof(Point) == a.GetType());
+
+public class Point
+{
+    public float X { get; }
+    public float Y { get; }
+    public Point(float x, float y)
+    {
+        X = x; Y = y;
+    }
 }
